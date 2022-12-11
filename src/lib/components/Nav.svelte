61a31792248ee9817284c6906921feafed609023
Anna <nilsson.j.anna@gmail.com>
2022-12-11 18:02:38 +0100

    Wiki now opens in a new tab

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -11,7 +11,7 @@
 			<NavLink href="/lunch" class="">Lunch</NavLink>
 		</li>
 		<li>
-			<NavLink href="https://wiki.dtek.se" class=""
+			<a href="https://wiki.dtek.se" target="_blank" rel="noreferrer"
 				>Wiki <svg
 					xmlns="http://www.w3.org/2000/svg"
 					class="h-5 w-5 feather feather-external-link"
@@ -24,8 +24,8 @@
 					><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" /><polyline
 						points="15 3 21 3 21 9"
 					/><line x1="10" y1="14" x2="21" y2="3" /></svg
-				></NavLink
-			>
+				>
+			</a>
 		</li>
 		<li>
 			<NavLink href="/about" class="">About</NavLink>
